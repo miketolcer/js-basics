7e921bcb64783f963ee56e6c0bad8987494731e6
miketolcer <miketolcer@yahoo.com>
2021-10-16 22:14:24 -0400

    draft

--- arrays.js
+++ arrays.js
@@ -4,11 +4,16 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+let colorCopy = faveColors.slice()
+  console.log(colorCopy)
+
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('yellow')
+  console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,6 +24,9 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
+let middleNums = numbers.slice(1, 4);
+  console.log(middleNums)
+
 
 
 //////////////////PROBLEM 4////////////////////
