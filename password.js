7d71ebfcbeca268555ff3057247267e2f514737d
miketolcer <miketolcer@yahoo.com>
2021-10-16 22:25:21 -0400

    draft

---
+++ password.js
@@ -0,0 +1,12 @@
+let userpresent = true;
+if (userpresent === true) {
+    console.log("welcome user")
+}
+if (userpresent === true){
+    console.log("password?")
+}
+const passWord = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+let [i] = array.length
+if i === 10
+    console.log ("success")
+    else console.log ("failure")
